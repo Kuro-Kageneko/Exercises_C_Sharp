4f79fb5bc6dee6443aea9724d05b0bf85992952e
Benjamin Gerhard <kuro.89@icloud.com>
2023-08-31 09:40:39 +0200

    Blubb

--- XX_Methods/Exercise_4.cs
+++ XX_Methods/Exercise_4.cs
@@ -6,12 +6,17 @@ namespace Exercises_C_Sharp.XX_Methods
         {
             //2. Rufen Sie die unten erstellte Methode auf und geben Sie den Inhalt auf der Konsole aus.
             //Code START
-            
+            Console.WriteLine(Begrüßung());
             //Code ENDE
         }
         //Erstellen Sie eine Methode, die bei jedem Aufruf zufällig eine von 10 Begrüßungen zurück gibt.
         //Code START
-        
+        static string Begrüßung()
+        {
+            Random rand = new Random();
+            string[] beg = new string[] {"Hi!", "Hey!", "Guten Tag!", "Herzlich Willkommen!", "Willkommen!", "Guten Morgen!", "Hallo!", "Huhu!", "Servus!", "NÖ"};
+            return beg[rand.Next(0, beg.Length)];
+        }
         //Code ENDE
 
     }
--- XX_Methods/Exercise_5.cs
+++ XX_Methods/Exercise_5.cs
@@ -6,12 +6,36 @@ namespace Exercises_C_Sharp.XX_Methods
         {
             //2. Fragen Sie den Nutzer mit Hilfe der unten erstellten Methode nach einer Primzahl. Fragen Sie diesen dann, ob er noch eine weitere Primzahl eingeben möchte. Wiederholen Sie den Vorgang, bis der Nutzer keine Zahl mehr eingeben möchte. Multiplizieren Sie dann alle Eingaben miteinander und geben das Ergebnis aus.
             //Code START
-            
+            int res = 1;    
+            while (true) 
+            {
+                Console.WriteLine("Bitte Primzahl eingeben:");
+                res *= primzahl();
+            }
             //Code ENDE
         }
         //Schreiben Sie eine Methode, bei der der Nutzer eine Primzahl (Zahl, die nur durch 1 und sich selbst teilbar ist) eingeben soll. Geben Sie diese Zahl zurück. Wenn der Nutzer eine falsche Eingabe tätigt, dann soll der Nutzer darüber informiert und die Abfrage wiederholt werden.
         //Code START
-        
+        static void primzahl() 
+        {
+            while (true)
+            {
+                int ui = Convert.ToInt32(Console.ReadLine());
+
+                bool b = true;
+                for (int i = 2; i < ui; i++)
+                {
+                    if (ui % 1 == 0)
+                    {
+                        //KEINE PRIMZAHL!
+                        b = false;
+                        break;
+                    }
+                }
+                if (b) return ui;
+                Console.WriteLine("Die eingegebene Zahl ist keine Primzahl. Please try again:");
+            }
+        }
         //Code ENDE
 
     }
